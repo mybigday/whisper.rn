cb6699a0bb91642f262fde2e2be02ffcf400f6be
Jhen <developer@jhen.me>
2023-03-21 15:28:07 +0800

    feat(jest): update mock

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -5,7 +5,10 @@ jest.mock('..', () => require('../../jest/mock'))
 test('Mock', async () => {
   const context = await initWhisper()
   expect(context.id).toBe(1)
-  expect(await context.transcribe('test.wav')).toEqual({ result: 'TEST' })
+  expect(await context.transcribe('test.wav')).toEqual({
+    result: ' Test',
+    segments: [{ text: ' Test', t0: 0, t1: 33 }],
+  })
   await context.release()
   await releaseAllWhisper()
 })
