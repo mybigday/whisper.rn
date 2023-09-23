965409ddcc153dc34845dea1559a5366632465ca
Jhen-Jie Hong <developer@jhen.me>
2023-09-23 14:29:10 +0800

    feat: implement VAD on realtime transcription (#129)
    * feat(ios): initial work for simple VAD
    
    * feat(ios): skip vad if isTranscribing
    
    * feat(ios): add vadMs / vadThold / vadFreqThold options
    
    * feat(android): implement vad on realtime transcription
    
    * feat: use vad to check last transcription
    
    * feat(example): do not use vad by default

--- cpp/rn-whisper.h
+++ cpp/rn-whisper.h
@@ -10,6 +10,7 @@ void rn_whisper_remove_abort_map(int job_id);
 void rn_whisper_abort_transcribe(int job_id);
 bool rn_whisper_transcribe_is_aborted(int job_id);
 void rn_whisper_abort_all_transcribe();
+bool rn_whisper_vad_simple(std::vector<float> & pcmf32, int sample_rate, int last_ms, float vad_thold, float freq_thold, bool verbose);
 
 #ifdef __cplusplus
 }
