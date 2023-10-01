634132f478905d0d43e9e418b273b6b7c47ff074
Jhen-Jie Hong <developer@jhen.me>
2023-10-01 13:26:36 +0800

    feat: refactor audio utils (#140)
    * feat(ios): move decodeWaveFile to audio utils
    
    * feat(android): create AudioUtils

--- ios/RNWhisper.mm
+++ ios/RNWhisper.mm
@@ -1,6 +1,7 @@
 #import "RNWhisper.h"
 #import "RNWhisperContext.h"
 #import "RNWhisperDownloader.h"
+#import "RNWhisperAudioUtils.h"
 #include <stdlib.h>
 #include <string>
 
@@ -122,7 +123,7 @@ RCT_REMAP_METHOD(transcribeFile,
     }
 
     int count = 0;
-    float *waveFile = [self decodeWaveFile:path count:&count];
+    float *waveFile = [RNWhisperAudioUtils decodeWaveFile:path count:&count];
     if (waveFile == nil) {
         reject(@"whisper_error", @"Invalid file", nil);
         return;
@@ -255,27 +256,6 @@ RCT_REMAP_METHOD(releaseAllContexts,
 {
     [self invalidate];
     resolve(nil);
-}
-
-- (float *)decodeWaveFile:(NSString*)filePath count:(int *)count {
-    NSURL *url = [NSURL fileURLWithPath:filePath];
-    NSData *fileData = [NSData dataWithContentsOfURL:url];
-    if (fileData == nil) {
-        return nil;
-    }
-    NSMutableData *waveData = [[NSMutableData alloc] init];
-    [waveData appendData:[fileData subdataWithRange:NSMakeRange(44, [fileData length]-44)]];
-    const short *shortArray = (const short *)[waveData bytes];
-    int shortCount = (int) ([waveData length] / sizeof(short));
-    float *floatArray = (float *) malloc(shortCount * sizeof(float));
-    for (NSInteger i = 0; i < shortCount; i++) {
-        float floatValue = ((float)shortArray[i]) / 32767.0;
-        floatValue = MAX(floatValue, -1.0);
-        floatValue = MIN(floatValue, 1.0);
-        floatArray[i] = floatValue;
-    }
-    *count = shortCount;
-    return floatArray;
 }
 
 - (void)invalidate {
