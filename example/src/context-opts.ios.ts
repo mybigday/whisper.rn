b0408b0fdece84894f7b6171e20cb8eb3d32cfc2
Jhen-Jie Hong <developer@jhen.me>
2024-11-11 11:48:11 +0800

    feat(example, ios): use metal by default (disable coreml)

--- example/src/context-opts.ios.ts
+++ example/src/context-opts.ios.ts
@@ -1,19 +1,21 @@
 import { Platform } from 'react-native'
 
+const getCoreMLModelAsset = () =>
+  Platform.OS === 'ios'
+    ? {
+        filename: 'ggml-base-encoder.mlmodelc',
+        assets: [
+          require('../assets/ggml-base-encoder.mlmodelc/weights/weight.bin'),
+          require('../assets/ggml-base-encoder.mlmodelc/model.mil'),
+          require('../assets/ggml-base-encoder.mlmodelc/coremldata.bin'),
+        ],
+      }
+    : undefined
+
 export default {
-  useCoreMLIos: true,
-  // If you don't want to enable Core ML, you can remove this property
-  coreMLModelAsset:
-    Platform.OS === 'ios'
-      ? {
-          filename: 'ggml-base-encoder.mlmodelc',
-          assets: [
-            require('../assets/ggml-base-encoder.mlmodelc/weights/weight.bin'),
-            require('../assets/ggml-base-encoder.mlmodelc/model.mil'),
-            require('../assets/ggml-base-encoder.mlmodelc/coremldata.bin'),
-          ],
-        }
-      : undefined,
-  useGpu: false, // Enable Metal (Will skip Core ML if enabled)
-  useFlashAttn: false,
+  useGpu: true, // Enable Metal (Will skip Core ML if enabled)
+  useFlashAttn: true,
+
+  useCoreMLIos: false,
+  coreMLModelAsset: getCoreMLModelAsset(),
 }
